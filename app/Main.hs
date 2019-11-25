82096a63332e62e6a08f793ce86c89816716dbe8
Jesse Williamson <Jesse@thunklife.local>
2019-11-24 16:22:21 -0800

    better

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,21 @@ module Main where
 
 import System.Random (randomRIO)
 
+getGuess :: IO Integer
+getGuess = readLn
+
+getRandom :: IO Integer
+getRandom = randomRIO (1, 100)
+
+getResponse :: Integer -> Integer -> String
+getResponse x y
+  | x == y = "You are very clever"
+  | otherwise = "Better luck next time. :("
+
 main :: IO ()
 main = do
   putStrLn "I'm thinking of a number between 1 and 100."
   putStrLn "Can you guess it?"
-  guess <- readLn :: IO Integer
-  rand <- randomRIO (1, 100) :: IO Integer
-  print $ guess == rand
+  guess <- getGuess
+  rand <- getRandom
+  print $ getResponse guess rand
